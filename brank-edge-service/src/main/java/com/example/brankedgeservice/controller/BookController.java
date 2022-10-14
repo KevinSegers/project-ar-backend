3fc64ce4e0c67762689e767fa5c097732e93ea04
KevinSegers <r0793406@student.thomasmore.be>
2022-10-14 13:17:46 +0200

    Base BookController.java

---
+++ brank-edge-service/src/main/java/com/example/brankedgeservice/controller/BookController.java
@@ -0,0 +1,19 @@
+package com.example.brankedgeservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class BookController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${pageservice.baseurl}")
+    private String pageServiceBaseUrl;
+
+    @Value("${itemservice.baseurl}")
+    private String itemServiceBaseUrl;
+}
