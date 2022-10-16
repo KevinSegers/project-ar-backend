aa259de5be113cc26e28f22aa8d2997da1f4088c
KevinSegers <r0793406@student.thomasmore.be>
2022-10-16 20:13:00 +0200

    bugfix wrong dependency 3 pom.xml

--- page-service/src/main/java/com/example/pageservice/repository/PageRepository.java
+++ page-service/src/main/java/com/example/pageservice/repository/PageRepository.java
@@ -1,13 +1,13 @@
 package com.example.pageservice.repository;
 
 import com.example.pageservice.model.Page;
-import org.springframework.data.mongodb.MongoRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PageRepository extends MongoRepository<Page, String>{
+public interface PageRepository extends MongoRepository<Page, String> {
     List<Page> findPagesByBookTitle(String bookTitle);
     List<Page> findPagesByItem(String ItemName);
     Page findPageByBookTitleAndPagenumber(String bookTitle, int pagenumber);
